487d446e7b4d79853740b03e75ecd474ff036725
IkennaGary <67379133+IkennaGary@users.noreply.github.com>
2024-11-19 08:54:26 +0100

    resolved grid bug

--- src/components/marketing/Home/Partners.tsx
+++ src/components/marketing/Home/Partners.tsx
@@ -14,19 +14,19 @@ const stats = [
 
 const Partners = () => {
   return (
-    <div className="bg-secondary-light pb-24 sm:pb-32">
+    <div className="bg-secondary-light pb-14 sm:pb-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <h5 className="text-xs uppercase text-center mb-10">our partners</h5>
-        <dl className="grid grid-cols-1 gap-x-8 gap-y-10 text-center items-center md:grid-cols-5">
+        <div className="flex gap-x-8 overflow-x-scroll snap-x snap-mandatory scrollbar-hide md:grid md:grid-cols-5 md:gap-y-10 md:gap-x-8">
           {stats.map((stat) => (
             <div
               key={stat.id}
-              className="mx-auto flex max-w-xs flex-col gap-y-4"
+              className="flex-shrink-0 snap-center mx-auto flex max-w-xs flex-col gap-y-4 justify-center"
             >
-              <img src={stat.value} alt="" className="w-14 md:w-20" />
+              <img src={stat.value} alt="" className="w-16 md:w-20" />
             </div>
           ))}
-        </dl>
+        </div>
       </div>
     </div>
   );
