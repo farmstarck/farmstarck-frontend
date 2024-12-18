8411087616cd53182feb068c68a7f8b6beb998db
IkennaGary <67379133+IkennaGary@users.noreply.github.com>
2024-12-18 09:20:46 +0100

    Updates

---
+++ assets/MarketPlace-BOzWU4zo.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-JQlydF3o.js";const r=()=>e.jsx("div",{children:"MarketPlacePage"});export{r as default};
---
+++ assets/benefit-img1-DHFKdU-H.js
@@ -0,0 +1,1 @@
+const s="/farmstarck-frontend/assets/benefit-img1-BPEfK2rG.svg";export{s as I};
